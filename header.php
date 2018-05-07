02890ccf1f00622dd0d7284abb628e9662c2d0ad
Carl Nielsen <nielsencs@users.noreply.github.com>
2018-05-07 13:01:46 +0100

    test

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+  <!-- from laptop -->
   <title>BibleStudyMan.co.uk</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
