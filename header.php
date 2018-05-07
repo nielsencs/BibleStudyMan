b62e01179f5dc7c7cd0fe043851f5a25c20af13d
Carl Nielsen <nielsencs@users.noreply.github.com>
2018-05-07 13:30:53 +0100

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-  <!-- from laptop -->
   <title>BibleStudyMan.co.uk</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
