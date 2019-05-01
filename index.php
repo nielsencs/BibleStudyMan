cdd8d40c25e8886ed31fa0cb89665dfc9be2346f
Carl Nielsen <nielsencs@users.noreply.github.com>
2019-05-01 15:01:50 +0100

    more/less on home page seperated out and generalised for page style change in teaching.php

--- index.php
+++ index.php
@@ -11,141 +11,103 @@
   $day = date("j", $timestamp);
 ?>
 
-<script>
-// $(document).ready(function(){
-//     $("#teachBlock").mouseenter(function(){
-//         $("#teachBlock").slideDown("fast");
-//     });
-//     $("#teachBlock").mouseleave(function(){
-//         $("#teachBlock").slideUp("fast");
-//     });
-// });
-</script>
+  <script type="text/javascript">
+  // $(document).ready(function(){
+  //     $("#block1").mouseenter(function(){
+  //         $("#block1").slideDown("fast");
+  //     });
+  //     $("#block1").mouseleave(function(){
+  //         $("#block1").slideUp("fast");
+  //     });
+  // });
+  </script>
 
-<script type="text/javascript">
-  var bFullHover = ! window.matchMedia("(hover:none), (hover:on-demand)").matches; // touchscreen without mouse gives false
-
-  window.onload = function(){
-    // if (bFullHover){
-      showHide('teachBlock', false);
-      showHide('readBlock', false);
-      showHide('planBlock', false);
-      showHide('bibleBlock', false);
-    // } else {
-    //   showHide('teachBlock', true);
-    //   showHide('readBlock', true);
-    //   showHide('planBlock', true);
-    //   showHide('bibleBlock', true);
-    // }
-  }
-
-  function showHide(tElemName, bShow){
-    elem = document.getElementById(tElemName);
-    if (bShow) {
-      elem.style.display = "block";
-      // elem.style.height = "auto";
-    } else {
-      elem.style.display = "none";
-      // elem.style.height = 0;
-    }
-  }
-
-  function showHide2(tElemName){
-    // alert(tElemName);
-    elem = document.getElementById(tElemName + 'Block');
-    button = document.getElementById(tElemName + 'Button');
-    if (elem.style.display == "none") {
-      // $('#teachBlock').slideDown("slow");
-      $('#' + tElemName + 'Block').slideDown("slow");
-      button.value = "Less"
-    } else {
-      // $('#teachBlock').slideUp("slow");
-      $('#' + tElemName + 'Block').slideUp("slow");
-      button.value = "More"
-    }
-  }
-</script>
+  <script src="showHide.js"></script>
 
   <div class='intro'>
-    <a href="about.php"><img class="mugshot" src="images/Carl-SmileSmall.png"
+    <a href="about.php"><img class="mugshot" src="images/Carl-Small.png"
       alt="mugshot of Carl"/></a>
-    <p>Welcome to the <strong>Bible Study Man</strong> website where I
+    <p class="frameborder">Welcome to the <strong>Bible Study Man</strong> website where I
       hope we can learn together what The Bible has to teach us. My aim
       is to bring this <strong>amazing book alive</strong> in a
-      <strong>fresh</strong> and <strong>interesting</strong> (sometimes
-      <strong>challenging</strong>) way!</p>
-    <p>I have divided the site into four key sections detailed below.
-      Feel free to explore and if there&rsquo;s anything you can&rsquo;t
-      find or understand - get in touch!</p>
+      <strong>fresh, interesting and</strong> sometimes
+      <strong>challenging</strong> way!</p>
+    <p class="frameborder">I have divided the site into <strong>four main sections</strong> detailed below.
+      Feel free to <strong>explore</strong> and if there&rsquo;s anything you can&rsquo;t
+      find or understand - <a href="mailto:carl@BibleStudyMan.co.uk">get in touch</a>!</p>
     <p class="signature">Carl Nielsen, January 2018</p>
   </div>
 
   <div class="main home">
     <h1>The Four Main Sections</h1>
     <!-- <a href="teaching.php" class="subMain teaching floatLeft"> -->
-    <div class="subMain teaching floatLeft">
-       <!-- onmouseover="showHide('teachBlock', true);" -->
-       <!-- onmouseout="showHide('teachBlock', false);"> -->
+    <div class="subMain teaching">
+       <!-- onmouseover="showHide('block1', true);" -->
+       <!-- onmouseout="showHide('block1', false);"> -->
       <h2>1. Online Teaching Sessions</h2>
-      <p>Do you reckon I&rsquo;ve learnt anything by reading The Bible from-cover-to-cover many times over several years?</p>
+      <p>Would you think I&rsquo;ve learnt anything by reading The Bible from-cover-to-cover many times over several years?</p>
       <!-- <p class="centerText">Read more...</p> -->
-      <input type="button" value="More" name="teachButton" id="teachButton" class="center" onclick="showHide2('teach');">
-      <div name="teachBlock" id="teachBlock" class="collapse">
+      <input type="button" value="More" name="button1" id="button1" class="center" onclick="showHide2('1');">
+      <div name="block1" id="block1" class="collapse">
         <p>I certainly hope I have! I also learnt from my time wrestling with
           clinical depression. What I&rsquo;ve come to call The Nielsen Creed has
           it&rsquo;s roots in that time:</p>
-        <p>God is God.</p>
-        <p>God is Good.</p>
-        <p>I am His.</p>
-        <p>And He is mine.</p>
+        <p class="centerText">God is God.</p>
+        <p class="centerText">God is Good.</p>
+        <p class="centerText">I am His.</p>
+        <p class="centerText">And He is mine.</p>
         <p>I unpack this and other subjects in this growing library of video and
           audio sessions.</p>
-        <!-- <p>On the live sessions interaction is encouraged.</p> -->
+        <p>On the live sessions interaction is encouraged.</p>
         <!-- <p class="centerText">Click to go there!</p> -->
         <input type="button" value="Go there!" class="center" onclick="window.location.href = 'teaching.php'">
       </div>
     </div>
     <!-- <a href="readings.php" class="subMain readings floatRight" -->
-    <div class="subMain readings floatRight">
-       <!-- onmouseover="showHide('readBlock', true);" -->
-       <!-- onmouseout="showHide('readBlock', false);"> -->
+    <div class="subMain readings">
+       <!-- onmouseover="showHide('block2', true);" -->
+       <!-- onmouseout="showHide('block2', false);"> -->
       <h2>2. Dramatic Bible Readings</h2>
       <p>Have you noticed that people often read The Bible without emotion?</p>
       <!-- <p class="centerText">Read more...</p> -->
-      <input type="button" value="More" name="readButton" id="readButton" class="center" onclick="showHide2('read');">
-      <div name="readBlock" id="readBlock" class="collapse">
+      <input type="button" value="More" name="button2" id="button2" class="center" onclick="showHide2('2');">
+      <div name="block2" id="block2" class="collapse">
         <p>There&rsquo;ll be none of that here! If you read the texts it is sometimes
           clear that the speaker is angry or sad, other times not so much. For example
           was Peter saying &ldquo;If YOU say so...&rdquo; or &ldquo;IF you SAY so...&rdquo;
           in Luke 5:5? We don't know but I feel that a little conjecture can help
           throw some light, providing we know we&rsquo;re guessing!</p>
-        <p>So these are my best guess (sometimes one of several &lsquo;best&rsquo;
+        <!-- p>So these are my best guess (sometimes one of several &lsquo;best&rsquo;
           guesses) intended to ring the changes and bring The Bible to life with
           full-on &rsquo;snot-and-tears&rsquo; readings. I doubt my interpretation
           accurately reflects the mood or emotional tone of the original, but I
-          think it&rsquo;s an interesting alternative to the usual formal reading style.</p>
+          think it&rsquo;s an interesting alternative to the usual formal reading style.</p -->
+        <p>So what I'm attempting is to bring The Bible to life with very expressive
+          readings. I doubt my interpretation accurately reflects the mood or emotional
+          tone of the original, but I think it&rsquo;s an interesting and sometimes
+          enlightening alternative to the usual formal reading style.</p>
         <!-- <p class="centerText">Click to go there!</p> -->
         <input type="button" value="Go there!" class="center" onclick="window.location.href = 'readings.php'">
       </div>
     </div>
     <!-- <a href="plan.php" class="subMain plan floatLeft" -->
-    <div class="subMain plan floatLeft">
-       <!-- onmouseover="showHide('planBlock', true);" -->
-       <!-- onmouseout="showHide('planBlock', false);"> -->
+    <div class="subMain plan">
+       <!-- onmouseover="showHide('block3', true);" -->
+       <!-- onmouseout="showHide('block3', false);"> -->
       <h2>3. Bible Reading Plan</h2>
       <p>Have you ever tried to read The Bible from cover to cover?</p>
       <!-- <p class="centerText">Read more...</p> -->
-      <input type="button" value="More" name="planButton" id="planButton" class="center" onclick="showHide2('plan');">
-      <div name="planBlock" id="planBlock" class="collapse">
-        <p>If not and you&rsquo;d like to, then this day-by-day plan I devised
-          will help you to read the entire Bible in one year. It takes less than
-          half an hour a day (even if you&rsquo;re a slow reader like me!)</p>
+      <input type="button" value="More" name="button3" id="button3" class="center" onclick="showHide2('3');">
+      <div name="block3" id="block3" class="collapse">
+        <p>If not and you&rsquo;d like to, then this day-by-day plan will help you
+          to read the entire Bible in one year. It takes less than half an hour
+          a day even if you&rsquo;re a slow reader like me!</p>
         <p>The plan is based on four traditional sections - The Law (Torah or Pentateuch);
           The Prophets (Neviim); The Writings or Poetry (Ktuviim) and The
           New Testament.</p>
 <?php
   if ($link) {
-    echo '<p>As a sample, today&rsquo;s bits to read are ' . daysReadingsAsSentence($month, $day) . '. Enjoy!</p>';
+    echo '<p>As a sample, today&rsquo;s sections to read are ' . daysReadingsAsSentence($month, $day) . '. Enjoy!</p>';
   }
 ?>
         <!-- p>If you'd rather read from your own Bible (there&rsquo;s nothing quite like a real book is there?) click here for <a href="planTable.php">the entire year&rsquo;s readings as a list.</a></p -->
@@ -155,18 +117,18 @@
       </div>
     </div>
     <!-- <a href="bible.php" class="subMain Bible floatRight" -->
-       <!-- onmouseover="showHide('bibleBlock', true);" -->
-       <!-- onmouseout="showHide('bibleBlock', false);"> -->
-    <div class="subMain Bible floatRight">
-       <!-- onmouseover="showHide('planBlock', true);" -->
-       <!-- onmouseout="showHide('planBlock', false);"> -->
+       <!-- onmouseover="showHide('block3', true);" -->
+       <!-- onmouseout="showHide('block3', false);"> -->
+    <div class="subMain Bible">
+       <!-- onmouseover="showHide('block3', true);" -->
+       <!-- onmouseout="showHide('block3', false);"> -->
       <h2>4. The Bible</h2>
       <p>Do you know the difference between &lsquo;God&rsquo;, &lsquo;
         <span class="small-caps">The Lord</span>&rsquo; (as opposed to &lsquo;The
         Lord&rsquo;) & &lsquo;God Almighty&rsquo;?</p>
       <!-- <p class="centerText">Read more...</p> -->
-      <input type="button" value="More" name="bibleButton" id="bibleButton" class="center" onclick="showHide2('bible');">
-      <div name="bibleBlock" id="bibleBlock" class="collapse">
+      <input type="button" value="More" name="button4" id="button4" class="center" onclick="showHide2('4');">
+      <div name="block4" id="block4" class="collapse">
         <p>No? neither do I! Well that's a bit strong; what I mean is, it's not
           obvious which relates to the original words for God; and the differences
           are interesting. I&rsquo;m more clued up about &lsquo;elohim&rsquo; -
