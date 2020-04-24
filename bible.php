0cf63635203c4f5ee43579926790d5846b9c7ace
Carl Nielsen <nielsencs@users.noreply.github.com>
2020-04-24 16:54:02 +0100

    Update bible.php
    Search box refined

--- bible.php
+++ bible.php
@@ -147,67 +147,44 @@
         <div class="main Bible">
             <h1>The Bible</h1>
             <div class="subMain sectGeneral">
-        <p>This is a minor adaptation of the <a href="https://worldenglishbible.org" target="_blank">WEB</a>
-          to include nuanced meanings of particular ancient words for placenames,
-          God and others of special interest. In general square brackets:[] are
-          used to indicated words not found in the original text. It also indicates
-          the 5 books of the Psalms; and a few passages considered by some to be
-          of questionable authenticity.</p>
-                <!-- p>This doesn't look like much yet...<br>
-                I need to develop the code to:</p>
-                <ul>
-                    <li>complete the compilation of this edition</li>
-                    <li>provide a lookup for certain words</li>
-                    <li>Any other requests?</li>
-                </ul -->
-
                 <form name="searchForm" id="searchForm" action="<?php echo filter_input(INPUT_SERVER, 'PHP_SELF');?>" method="get" onsubmit="showWait();">
 
                 <table class="searchTable">
                   <tbody>
                     <tr>
-                      <td><input type="reset" name="clearAll" id="clearAll" value="Clear All"></td>
-                      <td><input type="submit" value="Search"></td>
+                      <td colspan="2">
+                          Search by keyword or book or a combination<br />
+                        <!-- <td colspan="2"><input type="text" name="words" id="words" value="<?php echo $tWords; ?>"></td> -->
+                        <input type="text" name="words" id="words" placeholder="Enter phrase or word" value="">
+                        <!-- <input type="checkbox" name="showMore" id="showMore"<?php if($bShowMore){ echo ' checked';} ?>> Show More? -->
+                        <input type="button" value="x" onclick="clearField('words')">
+                        <input type="checkbox" name="showMore" id="showMore" onclick="doSubmit()"> Show More?
+                      </td>
                     </tr>
                     <tr>
                       <td>
+                        <input type="button" value="&lt;" onclick="doDirection('pb')">
                         Book
+                        <input type="button" value="&gt;" onclick="doDirection('nb')">
                         <br />
-                        <input type="text" name="book" id="book" value="" list="books">
-                        <datalist name="books" id="books">
-                        <!-- <td><select name="book" id="book"> -->
-                        <!-- <option value="">Pick a book</option>'; -->
+                        <!--<input type="text" name="book" id="book" value="" list="books">-->
+                        <!--<datalist name="books" id="books">-->
+                        <select name="book" id="book" onchange="doSubmit(true)"> 
+                         <option value="">Pick a book</option>'; 
 <?php
   echo prepareDropdownBookList();
 ?>
-                        </datalist>
-                        <!-- </select> -->
-                        <br />
-                        <input type="button" value="&lt;" onclick="doDirection('pb')">
-                        <input type="button" value="Clear" onclick="clearField('book')">
-                        <input type="button" value="&gt;" onclick="doDirection('nb')">
+                        <!--</datalist>-->
+                         </select> 
+                        <input type="button" value="x" onclick="clearField('book')">
                       </td>
                       <td>
-                        Chapter
-                        <br />
-                        <!-- <input type="text" name="chapter" id="chapter" value="<?php echo $tChapter; ?>"> -->
-                        <input type="text" name="chapter" id="chapter" value=""><br />
                         <input type="button" value="&lt;" onclick="doDirection('pc')">
-                        <input type="button" value="Clear" onclick="clearField('chapter')">
+                        Chapter
                         <input type="button" value="&gt;" onclick="doDirection('nc')">
-                      </td>
-                    </tr>
-                    <tr>
-                      <td colspan="2">Words<br />
-                        <input type="button" value="Clear" onclick="clearField('words')">
-                        <!-- <td colspan="2"><input type="text" name="words" id="words" value="<?php echo $tWords; ?>"></td> -->
-                        <input type="text" name="words" id="words" value="">
-                        <!-- <input type="checkbox" name="showMore" id="showMore"<?php if($bShowMore){ echo ' checked';} ?>> Show More? -->
-                        <input type="checkbox" name="showMore" id="showMore" onclick="doSubmit()"> Show More?
                         <br />
-                        &lsquo;Special&rsquo; Words<br />
-                        <input type="checkbox" name="highlightSW" id="highlightSW" checked="true" onclick="doSubmit(true)">Highlight
-                        <input type="checkbox" name="showOW" id="showOW" checked="true" onclick="doSubmit(true)">Show Hebrew/Greek
+                        <input type="number" name="chapter" id="chapter" value="" onchange="doSubmit(true)">
+                        <input type="button" value="x" onclick="clearField('chapter')">
                       </td>
                     </tr>
                     <tr>
@@ -216,10 +193,20 @@
                     </tr>
                   </tbody>
                 </table>
-                </form>
+                <p>For certain words where the meaning is difficult to translate,
+                  <input type="checkbox" name="highlightSW" id="highlightSW" checked onclick="doSubmit(true)">highlight them
+                  <input type="checkbox" name="showOW" id="showOW" onclick="doSubmit(true)">Show Hebrew/Greek
+                </p>
+              </form>
 <?php
   echo passage($tBook, $tChapter, $tVerses, $tWords, $bShowMore);
 ?>
+        <p>This is a minor adaptation of the <a href="https://worldenglishbible.org" target="_blank">WEB</a>
+          to include nuanced meanings of particular ancient words for placenames,
+          God and others of special interest. In general square brackets:[] are
+          used to indicated words not found in the original text. It also indicates
+          the 5 books of the Psalms; and a few passages considered by some to be
+          of questionable authenticity.</p>
             </div>
         </div>
 <?php
