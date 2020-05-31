aec58aaa75bc1854878abc308e8940730c59a6ae
Carl Nielsen <nielsencs@users.noreply.github.com>
2020-05-31 22:37:11 +0100

    minor speed improvement

--- site/header.php
+++ site/header.php
@@ -18,9 +18,9 @@
   <link rel="stylesheet" type="text/css" href="styles/general.css">
   <link rel="stylesheet" type="text/css" href="styles/pages.css">
   
-  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="scripts/jquery-3.5.1.min.js"></script>
   <script src='https://www.google.com/recaptcha/api.js'></script>
-
+  
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
@@ -45,9 +45,7 @@
 </head>
 
 <body>
-  <div id="waitHint" class="modal">
-    <!-- <img class="center" src="images/wait.gif" alt="Wait"> -->
-  </div>
+  <div id="waitHint" class="modal"></div>
   <div class="content">
     <noscript>
       <h1 class="centerText">It seems that you currently don&rsquo;t have Javascript enabled. To get the best from this site, you will want to enable it.</h1>
@@ -56,11 +54,8 @@
     <img class="banner-image" src="images/BibleBannerRainbow<?php if(! $bHome){echo 'Bot';} ?>.jpg" alt="pic of open Bible on desk">
 
     <div class="menu">
-      <!-- <div class="logo"> -->
-        <!-- <a href="https://www.BibleStudyMan.co.uk">BibleStudyMan</a> -->
-        <a href="https://www.BibleStudyMan.co.uk"><img class="logo" src="images/BSMLogo.png" alt="BibleStudyMan logo"></a>
-      <!-- </div> -->
-
+      <a href="https://www.BibleStudyMan.co.uk"><img class="logo" src="images/BSMLogo.png" alt="BibleStudyMan logo"></a>
+  
       <ul class="nav">
         <li><a href="index.php">Home</a></li>
         <li><a href="teaching.php">Teaching</a></li>
