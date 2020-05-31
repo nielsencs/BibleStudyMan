bd98a6c37041c16d0bf00312887970f9c5514b68
Carl Nielsen <nielsencs@users.noreply.github.com>
2020-05-31 22:40:06 +0100

    moved back!
    Sorry, Marvin!

--- site/sw.js
+++ site/sw.js
@@ -35,10 +35,22 @@ self.addEventListener('activate', evt => {
   );
 });
 // fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+//self.addEventListener('fetch', evt => {
+//  evt.respondWith(
+//    caches.match(evt.request).then(cacheRes => {
+//      return cacheRes || fetch(evt.request);
+//    })
+//  );
+//});
+
+addEventListener('fetch', event => {
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cachedResponse = await caches.match(event.request);
+    // Return it if we found one.
+    if (cachedResponse) return cachedResponse;
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
 });
