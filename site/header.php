e80723393eb28613d5a1d9df5679be2940a345a1
Carl Nielsen <nielsencs@users.noreply.github.com>
2020-05-31 22:52:31 +0100

    swap font for speed

--- site/header.php
+++ site/header.php
@@ -14,7 +14,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
   <link rel="stylesheet" type="text/css" href="styles/resetRichardClark.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Londrina+Solid:300">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Londrina+Solid:300&display=swap">
   <link rel="stylesheet" type="text/css" href="styles/general.css">
   <link rel="stylesheet" type="text/css" href="styles/pages.css">
   
