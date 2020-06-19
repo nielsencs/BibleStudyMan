2fa12111b2c43950eb08f3ea1081c75155abd473
Carl Nielsen <nielsencs@users.noreply.github.com>
2020-06-19 09:31:11 +0100

    mods to videoList() and videos added to db

--- site/dbFunctions.php
+++ site/dbFunctions.php
@@ -680,43 +680,46 @@ function videoList($tClass = 'R'){
   $tQuery .= 'FROM media ';
   $tQuery .= 'WHERE media.mediaClass = "' . $tClass . '";';
 
+  $tWidth = '420';
+  $tHeight = '315';
+
   $result = doQuery($link, $tQuery);
 
   if (mysqli_num_rows($result) == 0) {
     $tOutput .= 'Tell Carl something went wrong with the BibleStudyMan database - trying to do "' . $tQuery . '"';
   } else {
     while ($row = mysqli_fetch_assoc($result)) {
+      $tOutput .= '<div class="media">';
       if(! empty($row["audioURL"])){
-        $tOutput .= '<div class="media">';
         $tOutput .= $row["mediaName"];
         $tOutput .= '<br />';
-        $tOutput .= ' <a href="https://soundcloud.com/user-442938965/';
-        $tOutput .= $row["audioURL"];
-        $tOutput .= '">Play on SoundCloud.com';
-        $tOutput .= '</a> ';
-        $tOutput .= '<br />';
+//        $tOutput .= ' <a href="https://soundcloud.com/user-442938965/';
+//        $tOutput .= $row["audioURL"];
+//        $tOutput .= '">Play on SoundCloud.com';
+//        $tOutput .= '</a> ';
+//        $tOutput .= '<br />';
 
-        // $tOutput .= '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/
-        // $tOutput .= $row["audioTrack"];
-        // $tOutput .= '&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>';
-        $tOutput .= '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/';
+//        $tOutput .= '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/';
+        $tOutput .= '<iframe width="100%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/';
+//        $tOutput .= '<iframe scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/';
         $tOutput .= $row["audioTrack"];
         $tOutput .= '&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>';
         $tOutput .= '<br />';
       }
 
       if(! empty($row["videoURL"])){
-        $tOutput .= '<a href="https://youtu.be/';
-        $tOutput .= $row["videoURL"];
-        $tOutput .= '">Play on YouTube.com';
-        $tOutput .= '</a>';
-        $tOutput .= '<br />';
+//        $tOutput .= '<a class="centerText" href="https://youtu.be/';
+//        $tOutput .= $row["videoURL"];
+//        $tOutput .= '">Play on YouTube.com';
+//        $tOutput .= '</a>';
+//        $tOutput .= '<br />';
 
-        $tOutput .= '<iframe src="https://www.youtube.com/embed/';
+        $tOutput .= '<iframe width = "420" height = "315" src="https://www.youtube.com/embed/';
         $tOutput .= $row["videoURL"];
-        $tOutput .= '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-        $tOutput .= '</div>';
+//        $tOutput .= '?controls=1&modestbranding=0"';
+        $tOutput .= '?rel=0" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
       }
+      $tOutput .= '</div>';
     }
   }
   mysqli_free_result($result);
