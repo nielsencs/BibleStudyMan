3a03b4c09ff2c6eeaa391efd5362dea019ed403c
Carl Nielsen <nielsencs@users.noreply.github.com>
2020-06-21 16:12:57 +0100

    make CC icon local

--- site/footer.php
+++ site/footer.php
@@ -2,7 +2,7 @@
   <div>
     <a rel="noreferrer license" href="http://creativecommons.org/licenses/by-sa/4.0/"
       target="_blank"><img alt="Creative Commons License" style="border-width:0"
-      src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"/></a><br/>This work
+                           src="images/CC-BY-SA_88x31.png"/></a><br/>This work
       is licensed under a <a rel="noreferrer license" href="http://creativecommons.org/licenses/by-sa/4.0/"
       target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
   </div>
