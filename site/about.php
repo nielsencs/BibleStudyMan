b3450270c3fea2e71097ae77d7251df2f864ce8f
Carl Nielsen <nielsencs@users.noreply.github.com>
2020-06-21 15:17:32 +0100

    minor update - oh and depression gone!

--- site/about.php
+++ site/about.php
@@ -6,36 +6,42 @@
           <img class="mugshot" src="images/CarlSmall.png" alt="mugshot of Carl"/>
           <h2>So who is this Bible Study Man anyways?</h2>
             <p>Hi! My name is Carl Nielsen and I gave my life to The Lord on
-                14th September 1980. Sometime after that I began making a habit of
-                reading The Bible over a year each year.</p>
+              14th September 1980. Sometime after that I began making a habit of
+              reading The Bible over a year each year.</p>
             <p>Several years ago I was diagnosed with clinical depression and in
-                my worst days I could no longer manage those daily readings, but
-                what I had previously read was embedded in my heart and some days
-                all I had was "I know that my redeemer lives".
-                (<a href="bible.php?book=Job&chapter=19&verses=25">Job 19:25</a>).
-                I'm glad to say that depression is largely behind me now, but I
-                still have to win a daily victory over that tendency. Because of
-                my uncertainty as to what year I started and those dark days I
-                cannot be more precise, but I have certainly read The Bible from
-                cover-to-cover more than 20 times.</p>
-            <p>I am currently a member of the leadership team at
-                <a href="http://www.cleanslatechurch.co.uk/" target="_blank">CleanSlate Church</a>,
-                Milton Keynes, England where my responsibilities include overseeing
-                and teaching one of the discipleship programmes, leading the
-                Alpha course, worship and inner healing. I am also a Course Prayer Warrior with
-                <a href="http://www.wholenessthroughchrist.org" target="_blank">Wholeness Through Christ</a>
-                and previously served as a member of their board of trustees.</p>
+              my worst days I could no longer manage those daily readings, but
+              what I had previously read was embedded in my heart and some days
+              all I had was "I know that my redeemer lives".
+              (<a href="bible.php?book=Job&chapter=19&verses=25">Job 19:25</a>).
+              I'm glad to say that depression is largely behind me now, but until
+              recently I still had to win a daily victory over that tendency often
+              saying that &lsquo;It&rsquo;s a bit like the recovering alcoholic that
+              has to avoid alcohol altogether and the equivalent for the depression
+              is I have to watch my thought life extremely carefully&rsquo;. That
+              has not been my experience for several months now! By the grace of
+              God I can honestly say that depression is now no longer a part of
+              my life. Because of my uncertainty as to what year I started (and
+              those dark days) I cannot be more precise, but I have certainly read
+              The Bible from cover-to-cover more than 20 times.</p>
+            <p>I have served as an elder in a local church and was until 2019 a member 
+              of the leadership team at CleanSlate Church Milton Keynes, England
+              where my responsibilities included overseeing and teaching one of
+              the discipleship programmes, leading the Alpha courses, worship
+              and inner healing. I am also a Prayer Warrior with
+              <a href="http://www.wholenessthroughchrist.org" target="_blank">Wholeness Through Christ</a>
+              and previously served as a member of their board of trustees.</p>
             <p>So what? well I do believe that God has shown me by this continuous
-                exposure to his word some insights into how to "correctly handle the word of truth"
-                (<a href="bible.php?book=2+Timothy&chapter=2&verses=15">2 Timothy 2:15</a>).
-                I think there can also be a tendency among believers to assume that
-                whatever is spoken from the front must be right rather than to test
-                it for themselves
-                (<a href="bible.php?book=1+Thessalonians&chapter=5&verses=19-22">1 Thessalonians 5:19-22</a>).
-                So let's check what the word of God really says and learn "how
-                wide and long and high and deep is the love of Christ"
-                (<a href="bible.php?book=Ephesians&chapter=3&verses=18">Ephesians 3:18</a>)
-                for us together!</p>
+              exposure to his word some insights into how to "correctly handle the word of truth"
+              (<a href="bible.php?book=2+Timothy&chapter=2&verses=15">2 Timothy 2:15</a>).
+              I think there can also be a tendency among believers to assume that
+              whatever is spoken from the front must be right rather than to test
+              it for themselves
+              (<a href="bible.php?book=1+Thessalonians&chapter=5&verses=19-22">1 Thessalonians 5:19-22</a>).
+              So let's check what the word of God really says and learn "how
+              wide and long and high and deep is the love of Christ"
+              (<a href="bible.php?book=Ephesians&chapter=3&verses=18">Ephesians 3:18</a>)
+              for us together!</p>
+            <p class="signature">Carl Nielsen, June 2020</p>
         </div>
 
 <?php
