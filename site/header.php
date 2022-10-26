e1eeeb5519d52af6ef18675d593bc1ea2ae884b0
Carl Nielsen <nielsencs@users.noreply.github.com>
2022-10-26 12:54:44 +0100

    try without Osano

--- site/header.php
+++ site/header.php
@@ -10,17 +10,17 @@
   <title>BibleStudyMan.co.uk</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <link rel="stylesheet" type="text/css" href="styles/resetRichardClark.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Londrina+Solid:300&display=swap">
   <link rel="stylesheet" type="text/css" href="styles/general.css">
   <link rel="stylesheet" type="text/css" href="styles/pages.css">
-  
-  <script src="https://cmp.osano.com/AzZcqjS44O5M21NrU/dda2dc42-5678-4e89-b7d6-c6ea02b5d890/osano.js"></script>
-  
+
+  <!-- script src="https://cmp.osano.com/AzZcqjS44O5M21NrU/dda2dc42-5678-4e89-b7d6-c6ea02b5d890/osano.js"></script -->
+
   <script src="scripts/jquery-3.5.1.min.js"></script>
   <script src='https://www.google.com/recaptcha/api.js'></script>
-  
+
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
@@ -55,7 +55,7 @@
 
     <div class="menu">
       <a href="https://www.BibleStudyMan.co.uk"><img class="logo" src="images/BSMLogo.png" alt="BibleStudyMan logo"></a>
-  
+
       <ul class="nav">
         <li><a href="index.php">Home</a></li>
         <li><a href="teaching.php">Teaching</a></li>
