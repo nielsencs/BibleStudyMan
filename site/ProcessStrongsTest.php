fa73eb177270ac94cedb26a94730c1e9d175eed8
Carl Nielsen <nielsencs@users.noreply.github.com>
2024-06-20 18:49:47 +0100

    tidy indents and add scale

--- site/ProcessStrongsTest.php
+++ site/ProcessStrongsTest.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test ProcessStrongs</title>
-    <link rel="stylesheet" type="text/css" href="styles/resetRichardClark.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test ProcessStrongs</title>
+  <link rel="stylesheet" type="text/css" href="styles/resetRichardClark.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Londrina+Solid:300&display=swap">
   <link rel="stylesheet" type="text/css" href="styles/general.css">
   <link rel="stylesheet" type="text/css" href="styles/pages.css">
@@ -30,12 +30,13 @@ testIt('ForeverOne{H3068} God{H0430} planted a garden eastward, in Eden, and the
 testIt('He said, \"Blessed be ForeverOne{H3068}, the God{H0430} of Shem. Let Canaan be his servant.');
 
 function testIt($tTestVerse){
-    echo '<pre>' . $tTestVerse . '</pre>';
-    echo processStrongs($tTestVerse, true, true, true). '<br>';
-    echo processStrongs($tTestVerse, true, true, false). '<br>';
-    echo processStrongs($tTestVerse, true, false, true). '<br>';
-    echo processStrongs($tTestVerse, true, false, false). '<br>';
-    echo processStrongs($tTestVerse, false, false, true). '<br>';
-    echo processStrongs($tTestVerse, false, false, false). '<br>';
+  echo '<pre>' . $tTestVerse . '</pre>';
+  echo '<pre>' . '012345----10---+----20---+----30---+----40---+----50---+----60---+----70---+----80' . '</pre>';
+  echo processStrongs($tTestVerse, true, true, true). '<br>';
+  echo processStrongs($tTestVerse, true, true, false). '<br>';
+  echo processStrongs($tTestVerse, true, false, true). '<br>';
+  echo processStrongs($tTestVerse, true, false, false). '<br>';
+  echo processStrongs($tTestVerse, false, false, true). '<br>';
+  echo processStrongs($tTestVerse, false, false, false). '<br>';
     
 }
