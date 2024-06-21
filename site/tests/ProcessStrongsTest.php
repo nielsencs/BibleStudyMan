d0b8de2e8ad042a7a29a92ded8b445b785d649f2
Carl Nielsen <nielsencs@users.noreply.github.com>
2024-06-21 13:11:39 +0100

    let's put tests in a folder

--- site/tests/ProcessStrongsTest.php
+++ site/tests/ProcessStrongsTest.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Test ProcessStrongs</title>
-  <link rel="stylesheet" type="text/css" href="styles/resetRichardClark.css">
+  <link rel="stylesheet" type="text/css" href="../styles/resetRichardClark.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Londrina+Solid:300&display=swap">
-  <link rel="stylesheet" type="text/css" href="styles/general.css">
-  <link rel="stylesheet" type="text/css" href="styles/pages.css">
+  <link rel="stylesheet" type="text/css" href="../styles/general.css">
+  <link rel="stylesheet" type="text/css" href="../styles/pages.css">
 </head>
 <body>
     
@@ -15,7 +15,7 @@
 </html>
 
 <?php
-require_once 'dbFunctions.php';
+require_once '..\dbFunctions.php';
 $atStrongs = array();
 
 $atStrongs += ['H0430' => ['0', 'Elohim']];
@@ -25,8 +25,9 @@ $atStrongs += ['H3068' => ['1', 'Yahweh']];
 // H3068 Yahweh ForeverOne
 
 
-testIt('Oh ForeverOne{H3068} is God{H0430} and planted a garden eastward, in Eden, and there he put the man whom he had formed.');
+testIt('ForeverOne{H3068} God{H0430} planted a garden eastward, in Eden, and there he put the man whom he had formed.');
 testIt('He said, \"Blessed be ForeverOne{H3068}, the God{H0430} of Shem. Let Canaan be his servant.');
+testIt('God{H0430} is ForeverOne{H3068}.');
 
 function testIt($tTestVerse){
   echo '<br><br><pre>' . $tTestVerse . '</pre>';
