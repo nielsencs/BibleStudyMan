a331f8d101ddbc1f25646e82b9c36c6f83214fe4
Carl Nielsen <nielsencs@users.noreply.github.com>
2025-09-08 14:05:37 +0100

    Update search.php

--- site/search.php
+++ site/search.php
@@ -1,27 +1,32 @@
 <?php
-  $tBook = filter_input(INPUT_GET, 'book', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-  $tWords = trim(filter_input(INPUT_GET, 'words', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+  $tBook = filter_input(INPUT_GET, 'book', FILTER_UNSAFE_RAW);
+  $tWords = filter_input(INPUT_GET, 'words', FILTER_UNSAFE_RAW);
+  if($tWords > ''){
+    $tWords = trim($tWords);
+  }else{
+      $tWords = '';
+  }
   if(empty($tBook)){ // can't have a chapter without a book
     $tChapter = '';
     $tVerses = '';
   } else {
-    $tChapter = filter_input(INPUT_GET, 'chapter', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $tChapter = filter_input(INPUT_GET, 'chapter', FILTER_UNSAFE_RAW);
     if(empty($tChapter)){ // if chapter dropdown too small
-      $tChapter = filter_input(INPUT_GET, 'chapterNext', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+      $tChapter = filter_input(INPUT_GET, 'chapterNext', FILTER_UNSAFE_RAW);
     }
     if(empty($tChapter)){ // can't have verses without a chapter
       $tVerses = '';
     } else {
-        $tVerses = filter_input(INPUT_GET, 'verses', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+        $tVerses = filter_input(INPUT_GET, 'verses', FILTER_UNSAFE_RAW);
     }
   }
   $bExact = filter_input(INPUT_GET, 'exact', FILTER_DEFAULT, FILTER_FLAG_NO_ENCODE_QUOTES) === 'on';
-  $tMonth = filter_input(INPUT_GET, 'month', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-  $tDay = filter_input(INPUT_GET, 'day', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $tMonth = filter_input(INPUT_GET, 'month', FILTER_UNSAFE_RAW);
+  $tDay = filter_input(INPUT_GET, 'day', FILTER_UNSAFE_RAW);
   if(empty($tDay)){ // in case dropdown has too few days
-    $tDay = filter_input(INPUT_GET, 'dayNext', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $tDay = filter_input(INPUT_GET, 'dayNext', FILTER_UNSAFE_RAW);
   }
-  $tSortOrder = filter_input(INPUT_GET, 'sortOrder', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $tSortOrder = filter_input(INPUT_GET, 'sortOrder', FILTER_UNSAFE_RAW);
   if(empty($tBook . $tWords . $tMonth)){ // no search yet - 'clean' page
     $bHighlightSW = true;
     $bShowOW = true;
