1f5cd6a0bc99992f8251557d82e71a0a76f09956
Carl Nielsen <nielsencs@users.noreply.github.com>
2025-09-17 08:54:21 +0100

    Revert "neater"
    This reverts commit 9b9c00810365edd25d17702f4001fd4e0c0c32b9.

--- site/bible.php
+++ site/bible.php
@@ -29,10 +29,10 @@
                         <input type="search" name="words" id="words" placeholder="Enter phrase or word(s)" value="<?php echo htmlspecialchars($tWords, ENT_QUOTES, 'UTF-8'); ?>">
                         <input type="checkbox" name="exact" id="exact" <?php if($bExact){echo 'checked';}; ?>
                                onclick="doSubmit('words')"><label><abbr title="If this is checked you'll tend to
-                                                                                get fewer results as it treats the
-                                                                                words to the left as a phrase if
-                                                                                there are more than one or as the
-                                                                                exact word.">Exact</abbr></label>
+get fewer results as it treats the
+words to the left as a phrase if
+there are more than one or as the
+exact word.">Exact</abbr></label>
                       </td>
                     </tr>
                     <tr>
