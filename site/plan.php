651607599153efdd02ed5529154d23377506d38f
Carl Nielsen <nielsencs@users.noreply.github.com>
2025-09-22 10:12:39 +0100

    Update plan.php

--- site/plan.php
+++ site/plan.php
@@ -50,10 +50,10 @@
               <h1>The Bible Reading Plan</h1>
               <div class="subMain sectGeneral">
                 <h2>Readings for <?php echo htmlspecialchars(monthName($iMonth) . ' ' . $iDay, ENT_QUOTES, 'UTF-8'); ?>:</h2>
-  <?php
-    $tOutput = planTable($tSortOrder);
-    echo 'First, ' . daysReadingsAsSentence($iMonth, $iDay, $bHighlightSW, $bShowOW, $bShowTN);
-  ?>
+<?php
+  $tOutput = planTable($tSortOrder);
+  echo 'First, ' . daysReadingsAsSentence($iMonth, $iDay, $bHighlightSW, $bShowOW, $bShowTN);
+?>
                 <p>You can read the passages below. If you're looking to read for
                   a different day or want to use your own Bible, then
                 <a href="planTable">here&apos;s the entire year&apos;s plan
@@ -90,9 +90,8 @@
     echo daysReadingsAsVerses($iMonth, $iDay, $bHighlightSW, $bShowOW, $bShowTN);
     include_once 'bibleDisclaimer.html';
   ?>
-              </div>
-          </div>
-        </p>
+        </div>
+      </div>
     </div>
   </div>
 <?php
