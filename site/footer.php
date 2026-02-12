617d62217c56e736ad5c7a14372afd781236befa
Carl Nielsen <nielsencs@users.noreply.github.com>
2026-02-12 17:06:16 +0000

    indent and tidy

--- site/footer.php
+++ site/footer.php
@@ -1,33 +1,21 @@
-<div class="footer">
-  <div>
-    <a rel="noreferrer license" href="http://creativecommons.org/licenses/by-sa/4.0/"
-      target="_blank"><img alt="Creative Commons License" style="border-width:0"
-                           src="images/CC-BY-SA_88x31.png"/></a><br/>This work
-      is licensed under a <a rel="noreferrer license" href="http://creativecommons.org/licenses/by-sa/4.0/"
-      target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
-  </div>
+      <div class="footer">
+        <div>
+          <a rel="noreferrer license" href="http://creativecommons.org/licenses/by-sa/4.0/"
+            target="_blank"><img alt="Creative Commons License" style="border-width:0"
+                                src="images/CC-BY-SA_88x31.png"/></a><br/>This work
+            is licensed under a <a rel="noreferrer license" href="http://creativecommons.org/licenses/by-sa/4.0/"
+            target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+        </div>
 
-  <ul class="nav footNav">
-    <li><a rel="noreferrer" href="https://www.youtube.com/channel/UCdO1Yv6B9ijLBer0yLhCU3Q" target="_blank">YouTube</a></li> 
-    <!-- <li><a rel="noreferrer" href="https://www.twitter.com/nielsencs" target="_blank">Twitter</a></li> -->
-    <li><a rel="noreferrer" href="https://www.facebook.com/BibleStudyCarl/" target="_blank">Facebook</a></li>
-    <li><a rel="noreferrer" href="https://www.patreon.com/BibleStudyMan/" target="_blank">Patreon</a></li>
-    <li><a href="mailto:carl@BibleStudyMan.co.uk">Email</a></li> 
-    <li><a href="privacy">Privacy Policy</a></li>
-  </ul>
-</div>
-</div>
-<script type="text/javascript">
-// window.onpopstate = function(event) {
-//     document.getElementById('waitHint').style.display = 'none';
-// };
-// window.addEventListener('popstate', function(event) {
-//     document.getElementById('waitHint').style.display = 'none';
-// });
-// window.addEventListener('popstate', function(event) { 
-//   document.querySelector('.modal').style.display = 'none';
-// });
-</script>
-</body>
+        <ul class="nav footNav">
+          <li><a rel="noreferrer" href="https://www.youtube.com/channel/UCdO1Yv6B9ijLBer0yLhCU3Q" target="_blank">YouTube</a></li> 
+          <li><a rel="noreferrer" href="https://www.facebook.com/BibleStudyCarl/" target="_blank">Facebook</a></li>
+          <li><a rel="noreferrer" href="https://www.patreon.com/BibleStudyMan/" target="_blank">Patreon</a></li>
+          <li><a href="mailto:carl@BibleStudyMan.co.uk">Email</a></li> 
+          <li><a href="privacy">Privacy Policy</a></li>
+        </ul>
+      </div>
+    </div>
+  </body>
 </html>
-<?php $pdo = null;?>
+<?php $pdo = null; ?>
