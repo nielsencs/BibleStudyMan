23ec16abdb2a07084e6a27b2ba3f65ef2e2be08d
Carl Nielsen <nielsencs@users.noreply.github.com>
2026-02-12 18:00:15 +0000

    new class and tidy old <br />

--- site/intWords.php
+++ site/intWords.php
@@ -1,17 +1,18 @@
+<div class="searchOptions">
                 <p>For certain <abbr
                    title="Things like the various words for God
 and words that can have a variety of
 translations. For example in both
 Hebrew and Greek a certain word 
-can mean spirit or breath or breeze.">interesting</abbr> words:<br />
+can mean spirit or breath or breeze.">interesting</abbr> words:<br>
                   <input type="checkbox" name="highlightSW" id="highlightSW"
                   <?php if($bHighlightSW){echo 'checked';} ?>
-                       onclick="doSubmit()"><label><span class="highlightOW">highlight</span> them</label><br />
+                       onclick="doSubmit()"><label><span class="highlightOW">highlight</span> them</label><br>
                   <input type="checkbox" name="showOW"      id="showOW"
                   <?php if($bShowOW){echo 'checked';} ?>
-                       onclick="doSubmit()"><label>Show Hebrew/Greek</label><br />
+                       onclick="doSubmit()"><label>Show Hebrew/Greek</label><br>
                   <input type="checkbox" name="showTN"      id="showTN"
                   <?php if($bShowTN){echo 'checked';} ?>
-                       onclick="doSubmit()"><label>Show Translated Names</label><br />
-
+                       onclick="doSubmit()"><label>Show Translated Names</label><br>
                 </p>
+</div>
