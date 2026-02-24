292a750687a1f60feb6bae1ca4ec7de8e1d2df41
Carl Nielsen <nielsencs@users.noreply.github.com>
2026-02-24 17:08:03 +0000

    swap order of plan and Bible

--- site/controlPanel.php
+++ site/controlPanel.php
@@ -2,12 +2,36 @@
                 <form name="searchForm" id="searchForm" action="<?php if ($bBible) { echo'bible'; } else { echo'plan'; } ?>" method="get" onsubmit="showWait();">
 
                 <div id="searchSection">
-<?php if ($bBible) { ?>
                   <table class="searchTable">
-                    <tbody>
+<?php if ($bPlan) { ?>
+                    <tbody class="plan">
                       <tr>
                         <td colspan="2">
-                          Search by word or book or both<br>
+                          <select name="month" id="month" onchange="doSubmit('month')">
+<?php
+  echo prepareDropdownMonthList($iMonth);
+?>
+                          </select>
+                          <input type="button" value="&lt;" onclick="dayDirection('pd')">
+                          <input type="hidden" name="dayNext" id="dayNext" value="">
+                          <select name="day" id="day" onchange="doSubmit('day')">
+<?php
+  echo prepareDropdownDayList($iDay, $tMonth, $iDaysInMonth);
+?>
+                          </select>
+                          <input type="button" value="&gt;" onclick="dayDirection('nd')">
+                        </td>
+                        <td>
+                          <input type="button" value="Today" onclick="dayDirection('today')">
+                        </td>
+                      </tr>
+                    </tbody>
+<?php } ?>
+<?php if ($bBible) { ?>
+                    <tbody class="Bible">
+                      <tr>
+                        <td colspan="3">
+                          <!-- Search by word or book or both<br> -->
                           <input type="search" name="words" id="words" placeholder="Enter phrase or word(s)" value="<?php echo htmlspecialchars($tWords ?? '', ENT_QUOTES, 'UTF-8'); ?>">
                           <br>
                           <input type="checkbox" name="exact" id="exact" <?php if($bExact){echo 'checked';}; ?>
@@ -19,7 +43,7 @@ exact word.">Exact</abbr></label>
                         </td>
                       </tr>
                       <tr>
-                        <td colspan="2">
+                        <td colspan="3">
                           <input type="button" name="clearAll" id="clearAll" value="Clear" onclick="clearAllFields('bible')">
                           &nbsp;&nbsp;&nbsp;
                           <input type="submit" value="Search">
@@ -32,18 +56,14 @@ exact word.">Exact</abbr></label>
 You can select one of them here.">Book</abbr>&nbsp;
                           <input type="button" value="&gt;" onclick="doDirection('nb')">
                           <br>
-                          <!--<input type="text" name="book" id="book" value="" list="books">-->
-                          <!--<datalist name="books" id="books">-->
                           <select name="book" id="book"  onchange="doSubmit('book')">
                            <option value=""></option>
 <?php
   echo prepareDropdownBookList();
 ?>
-                          <!--</datalist>-->
                           </select>
-                          <!--<input type="button" value="Clear" onclick="clearField('book')">-->
                         </td>
-                        <td>
+                        <td colspan="2">
                           <input type="button" value="&lt;" onclick="doDirection('pc')">
                           &nbsp;<abbr title="The books in The Bible are divided
 into chapters; once you&apos;ve picked a
@@ -57,38 +77,15 @@ book, you can pick a chapter here.">Chapter</abbr>&nbsp;
   echo prepareDropdownChapterList();
 ?>
                           </select>
-                          <!--<input type="button" value="Clear" onclick="clearField('chapter')">-->
                         </td>
                       </tr>
+<?php } ?>
                     </tbody>
                   </table>
-<?php } ?>
-<?php if ($bPlan) { ?>
-                <table class="searchTable">
-                  <tr>
-                    <td>
-                      <select name="month" id="month" onchange="doSubmit('month')">
-<?php
-  echo prepareDropdownMonthList($iMonth);
-?>
-                      </select>
-                    </td>
-                    <td>
-                      <input type="button" value="&lt;" onclick="dayDirection('pd')">
-                      <input type="hidden" name="dayNext" id="dayNext" value="">
-                      <select name="day" id="day" onchange="doSubmit('day')">
-<?php
-  echo prepareDropdownDayList($iDay, $tMonth, $iDaysInMonth);
-?>
-                      </select>
-                      <input type="button" value="&gt;" onclick="dayDirection('nd')">
-                      <input type="button" value="Today" onclick="dayDirection('today')">
-                    </td>
-                  </tr>
-                </table>
-<?php } ?>
                 </div>
+<?php if ($bBible) { ?>
                 <input type="hidden" name="verses" id="verses" value="<?php if ($tBook > ''){echo htmlspecialchars($tVerses ?? '', ENT_QUOTES, 'UTF-8');} ?>">
+<?php } ?>
 <?php require_once 'intWords.php'; ?>
               </form>
             </div><!-- controlPanel -->
