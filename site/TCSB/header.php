b08af6e5bb3514f1d68b1e01ccd1fc497a21c584
Carl Nielsen <nielsencs@users.noreply.github.com>
2026-04-10 12:58:37 +0100

    Lexend font and breathing space

--- site/TCSB/header.php
+++ site/TCSB/header.php
@@ -34,7 +34,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="stylesheet" type="text/css" href="../styles/resetRichardClark.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Londrina+Solid:300&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap">
+
   <link rel="stylesheet" type="text/css" href="../styles/general.css">
   <link rel="stylesheet" type="text/css" href="../styles/input.css">
   <link rel="stylesheet" type="text/css" href="../styles/pages.css">
