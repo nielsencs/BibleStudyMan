9e9ed45ec6301b8cbc20c24cf24763b58e0a1d96
Carl Nielsen <nielsencs@users.noreply.github.com>
2026-04-28 15:29:30 +0100

    good change but let's move it to develop branch

--- site/footer.php
+++ site/footer.php
@@ -6,6 +6,7 @@
       is licensed under a <a rel="noreferrer license" href="http://creativecommons.org/licenses/by-sa/4.0/"
       target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
   </div>
+  
   <ul class="nav footNav">
     <li><a rel="noreferrer" href="https://www.youtube.com/channel/UCdO1Yv6B9ijLBer0yLhCU3Q" target="_blank">YouTube</a></li> 
     <!-- <li><a rel="noreferrer" href="https://www.twitter.com/nielsencs" target="_blank">Twitter</a></li> -->
