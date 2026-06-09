6600741eb8cf7720f0b8bb4cf71b67409c70bdb8
Carl Nielsen <nielsencs@users.noreply.github.com>
2026-06-09 21:24:08 +0100

    Update bible.php

--- site/bible.php
+++ site/bible.php
@@ -2,7 +2,7 @@
   require_once 'header.php';
 ?>
         <div class="main Bible">
-            <h1><?php echo $bFloaty ? 'The Bible' : 'The CleanSlate Bible'; ?></h1>
+            <h1>'The CleanSlate Bible'</h1>
             <div class="subMain sectGeneral">
 <?php if (! $bFloaty) { require_once 'controlPanel.php'; } ?>
 <?php
